ecb916a4d115c8e12300b8d9f2014286ed6856f3
nuoztu <nuoztu@gmail.com>
2021-05-25 18:23:48 -0500

    things we have done so far

---
+++ src/test/java/BranchingPractice.java
@@ -0,0 +1,15 @@
+public class BranchingPractice {
+
+    public static void main(String[] args) {
+
+        //create new project
+        //enable version control
+        //added .gitignore with .idea folder, target folder and .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
+
+    }
+
+
+
+}
